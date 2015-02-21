9ce15ed209520fd3d0f9fadb2b8ed9ac04111cdf
Jackie Loven <jloven@wpi.edu>
2015-02-20 23:54:34 -0500

    THE RICK ROLL WORKS OMG

--- trying_again.ino
+++ trying_again.ino
@@ -1,9 +1,24 @@
-
 int pin = 7;
+
+//  Example song
 int songLength = 3;
 int notes[] = {440, 659, 440};
 float durations[] = {1.0, 5.0, 1.0};
 int bpm = 180;
+
+// Example midi song
+//  define lengthOfMidi 3
+int lengthOfMidi = 3;
+int midi[] = {50, 60, 70};
+int hertz[] = {0, 0, 0};
+
+// RICK ROLL
+int lengthOfRickRoll = 27;
+int midiRick[] = {65, 67, 70, 67, 74, 74, 72, 65, 67, 70, 67, 72, 72, 70, 69, 67, 65, 67, 70, 67, 70, 72, 69, 65, 65, 72, 70};
+float durationsRick[] = {0.25, 0.25, 0.25, 0.25, 0.75, 0.75, 1.5, 0.25, 0.25, 0.25, 0.25, 0.75, 0.75, 0.75, 0.25, 0.5, 0.25, 0.25, 0.25, 0.25, 1.0, 0.5, 0.75, 0.25, 1.0, 0.5, 1.0, 2.0};
+int bpmRick = 120;
+//  Empty hertz for Rick Rolling.
+int hertzRick[27] = {};
 
 void playSong(int notes[], float durations[], int bpm, int songLength) {
  //  It's gonna take an array of notes, an array of durations, 
@@ -16,14 +31,23 @@ void playSong(int notes[], float durations[], int bpm, int songLength) {
  Serial.begin(9600);
  
  for (int i = 0; i < songLength; i ++) {
-   tone(pin, notes[i], (long) (durations[i] * bps) - 10);
-   delay((long) (durations[i] * bps));
-   Serial.print("note: ");
-   Serial.print(notes[i]);
-   Serial.print("duration: ");
-   Serial.println(durations[i] * bps - 10);
-   //  Play each note for this long, buffer of 10 to distinguish notes
+   tone(pin, notes[i], (long) (durations[i] * bps) - 10); //  Tone takes a pin number, an int note, and a long duration.
+   //  The 10 seconds is so consecutive same notes are distinguishable.
+   
+   delay((long) (durations[i] * bps)); //  DELAY OR THIS DOESN'T WORK! Tone is asynchronous- it has effects while the program is running
+   //  even if tone() already returned a value. You gotta leave it time to finish having those effects - i.e. a tone length.
  }
+}
+
+void midiToHertz(int midi[], int hertz[], int lengthOfMidi) {
+  for (int i = 0; i < lengthOfMidi; i ++) {
+    //  To convert midi to hertz equation: hertz = (2^(midi - 69)/12) * 440
+    hertz[i] = (int) (pow(2.0, ((midi[i] - 69.0) / 12.0)) * 440);
+  }
+  
+  for (int i = 0; i < lengthOfMidi; i ++) {
+    Serial.println(hertz[i]);
+  }
 }
 
 void setup() {
@@ -31,5 +55,6 @@ void setup() {
 }
 
 void loop() {
-  playSong(notes, durations, bpm, songLength);
+  midiToHertz(midiRick, hertzRick, lengthOfRickRoll);
+  playSong(hertzRick, durationsRick, bpmRick, lengthOfRickRoll);
 }
