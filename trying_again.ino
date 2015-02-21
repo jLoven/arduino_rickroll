99ae215cb00ff6274bfa43cc4c68c7712fa207cf
Jackie Loven <jloven@wpi.edu>
2015-02-20 23:06:25 -0500

    IT WORKS- DURATION AND THE NOTES ARRAYS WORK

--- trying_again.ino
+++ trying_again.ino
@@ -2,7 +2,7 @@
 int pin = 7;
 int songLength = 3;
 int notes[] = {440, 659, 440};
-float durations[] = {1.0, 5.0, 7.0};
+float durations[] = {1.0, 5.0, 1.0};
 int bpm = 180;
 
 void playSong(int notes[], float durations[], int bpm, int songLength) {
@@ -11,12 +11,13 @@ void playSong(int notes[], float durations[], int bpm, int songLength) {
  
  //  notes[] means just a pointer to notes, it has no idea what's in notes.
  
- float bps = (bpm / 60.0) * 1000.0; // Beats per second in milliseconds for arduino
+ float bps = (60.0 / bpm) * 1000.0; // Beats per second in milliseconds for arduino
  
  Serial.begin(9600);
  
  for (int i = 0; i < songLength; i ++) {
-   tone(pin, notes[i], (long) (durations[i] * bps - 10));
+   tone(pin, notes[i], (long) (durations[i] * bps) - 10);
+   delay((long) (durations[i] * bps));
    Serial.print("note: ");
    Serial.print(notes[i]);
    Serial.print("duration: ");
